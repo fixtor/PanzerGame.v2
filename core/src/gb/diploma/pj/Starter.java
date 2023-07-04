95506eec05ec4fa4c860bb73a4db532f78f6b73c
Igor Prusov <usovigor75@yandex.ru>
2023-07-04 18:59:50 +0300

    Todo rotate enemies 24 m videos

--- core/src/gb/diploma/pj/Starter.java
+++ core/src/gb/diploma/pj/Starter.java
@@ -3,10 +3,13 @@ package gb.diploma.pj;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Starter extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -20,7 +23,12 @@ public class Starter extends ApplicationAdapter {
 		Gdx.input.setInputProcessor(inputProccessor);
 		batch = new SpriteBatch();
 		me = new Panzer(100, 200);
-
+		List<Panzer> newEnemies = IntStream.range(0, 15).mapToObj(i -> {
+										int x = MathUtils.random(Gdx.graphics.getWidth());
+										int y = MathUtils.random(Gdx.graphics.getHeight());
+										return new Panzer(x, y, "enemy-right.png");
+										}).collect(Collectors.toList());
+		enemies.addAll(newEnemies);
 	}
 	/*Метод отрисовки кадра */
 	@Override
@@ -30,6 +38,8 @@ public class Starter extends ApplicationAdapter {
 		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
 		me.render(batch);
+		enemies.forEach(enemy -> {enemy.render(batch);});
+
 		batch.end();
 	}
 	/*Метод очистки памяти, удаления приложения */
